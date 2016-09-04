6e01bdbfc3b3bb44fb45eb78dcf64343caf1490e
Thiago Paes <mrprompt@gmail.com>
2016-09-04 01:46:02 -0300

    Update ponto.js

--- js/ponto/ponto.js
+++ js/ponto/ponto.js
@@ -16,7 +16,7 @@
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 var Ponto = {
-    apiServer: 'https://ponto-api-mrprompt.c9.io',
+    apiServer: 'https://ponto-mrprompt.rhcloud.com',
 
     /**
      * Cria o ambiente
